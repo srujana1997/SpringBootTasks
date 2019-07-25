4795b177626229d7031002d5fcb03d68f0d7d60f
srujana1997 <srujanadaruga@gmail.com>
2019-07-25 11:34:39 +0530

    application_properties

--- src/main/java/com/stackroute/Muzix/Track.java
+++ src/main/java/com/stackroute/Muzix/Track.java
@@ -2,12 +2,15 @@ package com.stackroute.Muzix;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.PropertySource;
+
 import javax.persistence.*;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@PropertySource("com.stackroute.resources")
 public class Track {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
