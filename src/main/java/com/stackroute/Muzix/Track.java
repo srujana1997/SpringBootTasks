a3d5fa71b76f06954a900bcf6ffa6ebe10c7a791
srujana1997 <srujanadaruga@gmail.com>
2019-07-25 11:03:05 +0530

    lombok

--- src/main/java/com/stackroute/Muzix/Track.java
+++ src/main/java/com/stackroute/Muzix/Track.java
@@ -1,65 +1,18 @@
 package com.stackroute.Muzix;
-
-import com.fasterxml.jackson.annotation.JsonTypeId;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 @Entity
-//@Table(name="Track")
-//@DataAmount
-//@NoArgsConstructor
-//@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Track {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column
     private int id;
-    @Column
     private String name;
-    @Column
     private String comment;
 
-    public Track(int id, String name, String comment) {
-        this.id = id;
-        this.name = name;
-        this.comment = comment;
-    }
-
-    public  Track(){
-
-    }
-    //setter and getter injections
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    @Override
-    public String toString() {
-        return "Track{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", comment='" + comment + '\'' +
-                '}';
-    }
 }
