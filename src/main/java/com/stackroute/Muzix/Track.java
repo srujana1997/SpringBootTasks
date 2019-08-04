5053e9f63231054396537998a1cd4448e9238653
srujana1997 <51950151+srujana1997@users.noreply.github.com>
2019-08-04 18:44:10 +0530

    Update Track.java

--- src/main/java/com/stackroute/Muzix/Track.java
+++ src/main/java/com/stackroute/Muzix/Track.java
@@ -3,16 +3,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import javax.persistence.*;
-
+//@Entity used to mark it as a database entity
 @Entity
+//Lombok plugin automatically generates getters, setters and constructors for any class marked as @Data
+@Data
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Track {
+    //annotated with @Id to make it primary key in database
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
-    private String name;
-    private String comment;
+    private int id; //track's id
+    private String name;//track's name
+    private String comment;//track's comment
 
 }
