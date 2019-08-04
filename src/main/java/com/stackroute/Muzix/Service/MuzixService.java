6e153272600ddde22354a8eb9eba001757be0741
srujana1997 <51950151+srujana1997@users.noreply.github.com>
2019-08-04 17:31:36 +0530

    Update MuzixService.java

--- src/main/java/com/stackroute/Muzix/Service/MuzixService.java
+++ src/main/java/com/stackroute/Muzix/Service/MuzixService.java
@@ -6,11 +6,16 @@ import com.stackroute.Muzix.Track;
 
 import java.util.List;
 import java.util.Optional;
-
+//interface for the service class
 public interface MuzixService {
+     //method to save a track
     public boolean saveTrack(Track track) throws TrackExist;
+     //method to delete a track
     public boolean deleteTrack(int Id) throws TrackNotFound;
+     //method to get track by id
     public Optional<Track> getTrackById(int trackId) throws TrackNotFound;
+     //method to get all tracks
     public List<Track> getAllTracks();
+     //method to update a track
     public boolean updateTrack(Track track,int id) throws TrackNotFound;
 }
