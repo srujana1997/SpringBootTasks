642085f8e50bcb6fdb03627dda51c03936314c9c
srujana1997 <51950151+srujana1997@users.noreply.github.com>
2019-08-04 18:24:26 +0530

    Update MuzixRepository.java

--- src/main/java/com/stackroute/Muzix/Repository/MuzixRepository.java
+++ src/main/java/com/stackroute/Muzix/Repository/MuzixRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+//Repository class used to perform database operations, extends JpaRepository
+//JpaRepository<EntityClassName,WrapperTypeOfIdPropertyInEntityClass>
 @Repository
 public interface MuzixRepository extends JpaRepository<Track,Integer> {
+    //Query to search a track by its name or comments in the database
     @Query(value = "select * from Track where name=?1",nativeQuery = true)
     List<Track> findTitleByName(String name);
 }
