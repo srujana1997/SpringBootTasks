9fade857c3dfab225c8e8712c79b5c69159abd8f
srujana1997 <51950151+srujana1997@users.noreply.github.com>
2019-08-04 18:48:36 +0530

    Update Track.java

--- src/main/java/com/stackroute/Muzix/domain/Track.java
+++ src/main/java/com/stackroute/Muzix/domain/Track.java
@@ -6,17 +6,19 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.persistence.*;
-
+//@Document used to mark it as a database document in mongoDb
 @Document
+//Lombok plugin automatically generates getters, setters and constructors for any class marked as @Data
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @PropertySource("com.stackroute.resources")
 public class Track {
+    //annotated with @Id to make it primary key in database
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
-    private String name;
-    private String comment;
+    private int id;//track's id
+    private String name;//track's name
+    private String comment;//track's comment
 
 }
