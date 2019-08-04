369783f9c4267a4f2bcaa161fd8e2e1aa505862d
srujana1997 <51950151+srujana1997@users.noreply.github.com>
2019-08-04 18:17:47 +0530

    Update MuzixRepository.java

--- src/main/java/com/stackroute/Muzix/Repository/MuzixRepository.java
+++ src/main/java/com/stackroute/Muzix/Repository/MuzixRepository.java
@@ -3,7 +3,8 @@ package com.stackroute.Muzix.Repository;
 import com.stackroute.Muzix.Track;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+//Repository class used to perform database operations, extends JpaRepository
+//JpaRepository<EntityClassName,WrapperTypeOfIdPropertyInEntityClass>
 @Repository
 public interface MuzixRepository extends JpaRepository<Track,Integer> {
 
