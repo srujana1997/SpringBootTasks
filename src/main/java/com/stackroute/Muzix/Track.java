ca36fe2367df80b68932fbf5f1f21fc69a0e0694
srujana1997 <51950151+srujana1997@users.noreply.github.com>
2019-08-04 18:40:27 +0530

    Update Track.java

--- src/main/java/com/stackroute/Muzix/Track.java
+++ src/main/java/com/stackroute/Muzix/Track.java
@@ -5,17 +5,19 @@ import lombok.NoArgsConstructor;
 import org.springframework.context.annotation.PropertySource;
 
 import javax.persistence.*;
-
+//@Entity used to mark it as a database entity
 @Entity
+//Lombok plugin automatically generates getters, setters and constructors for any class marked as @Data
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @PropertySource("com.stackroute.resources")
 public class Track {
+    //annotated with @Id to make it primary key in database
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
-    private String name;
-    private String comment;
+    private int id; //track's id
+    private String name;    //track's name
+    private String comment; //track's comments
 
 }
